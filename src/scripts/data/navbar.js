c5d840cd89d286a8c1bbd87719c116d7a8ac5d59
Fajar Febriansyah <91662079+fajar2302@users.noreply.github.com>
2022-12-09 15:16:29 +0800

    update navbar.js
    update navbar yang baru

--- src/scripts/data/navbar.js
+++ src/scripts/data/navbar.js
@@ -4,39 +4,34 @@ class Navbar extends HTMLElement {
   }
 
   render() {
-    this.innerHTML = `<!-- Awal dari header -->
-    <header>
-      <!-- Awal dari navigator -->
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <div class="navbar-brand">
-            <img src="../public/company-logo.png" class="img-fluid company-image" alt="company-logo" />
-            <span>Peduli Lingkungan</span>
-          </div>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="#">Beranda</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" href="#">Tentang Sampah</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Jenis Sampah</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link">Kami</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <!-- Akhir dari navigator -->
-    </header>
-    <!-- Akhir dari header -->`;
+    this.innerHTML = `
+    <nav class="navbar navbar-expand-lg fixed-top visible px-3">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="/images/page-1/logo.svg" alt="Logo" width="50" height="50" class=" ">
+                    <span class="brand-text fs-3">PeduliLingkungan</span>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link py-3 fs-6" aria-current="page" href="#/">Beranda</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link py-3 fs-6" href="#/tentang-sampah">Tentang Sampah</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link py-3 fs-6" href="#/jenis-sampah">Jenis Sampah</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link py-3 fs-6" href="#/kami">Kami</a>
+                        </li>
+                    </ul>
+                </div>`;
   }
 }
 
